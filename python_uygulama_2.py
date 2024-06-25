0b87127b88f3360dfbca89daf727bc9a04057f4d
ladycerfit <131827252+ladycerfit@users.noreply.github.com>
2024-06-25 21:54:28 +0300

    Update python_uygulama_2.py

--- python_uygulama_2.py
+++ python_uygulama_2.py
@@ -1,1 +1,22 @@
+##Noktaların Tanımlanması (points list):
 
+points = [(1, 2), (4, 6), (5, 7), (8, 10)]
+
+##Öklid Mesafesi İçin Bir Fonksiyon Yazma (eucledianDistance):
+##euclidean_distance = ((x2 – x1)^2 + (y2 – y1)^2)^1/2
+
+def euclideanDistance(point1, point2):
+    return ((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) ** 0.5
+
+##Mesafelerin Hesaplanması (distances list):
+
+distances = []
+for x in range(len(points)):
+    for y in range(x + 1, len(points)):
+        distance = euclideanDistance(points[x], points[y])
+        distances.append(distance)
+
+##Minimum Mesafenin Bulunması (min ve print):  
+      
+min_dis = min(distances)
+print(f"Minimum Mesafe: {min_dis:}")
